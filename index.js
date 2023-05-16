95ebc19fc2d0ea19da9c6cdb8b96434bac9c6c90
ozi <ozisadiqkasai@gmail.com>
2023-05-16 09:27:58 +0100

    modified files

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 }))
 
 
+// Smooth Scroll
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
